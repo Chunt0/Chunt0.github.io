2b890d85c00d168b8a3e4f69fb649b29b54705c1
Chunt0 <chunt.mail@gmail.com>
2024-07-12 12:47:52 -0700

    2024-07-12 12:47:52

---
+++ scripts/slotmachine.js
@@ -0,0 +1,44 @@
+$.fn.pause = function(duration) {
+    $(this).animate({ dummy: 1 }, duration);
+    return this;
+};
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function typeText(element, text, delay) {
+    let index = 0;
+    function type() {
+        if (index < text.length) {
+            element.textContent += text[index];
+            index++;
+            setTimeout(type, delay);
+        }
+    }
+    type();
+}
+
+$(document).ready(function() {
+    const typingDelay = 10;
+    
+    shuffle(images);
+    const imageList = $("#imageList");
+    imageList.empty();
+
+    const selectedImages = images.slice(0, 30);
+    selectedImages.forEach(function(src, index) {
+        const listItem = $('<li></li>');
+        const imgElement = $('<img>').attr('src', src).attr('alt', 'Img').addClass('small-img');
+        const captionElement = $('<div></div>').addClass('captions');
+        listItem.append(imgElement).append(captionElement);
+        imageList.append(listItem);
+        
+        if (index < selectedImages.length) {
+            typeText(captionElement[0], "    ", typingDelay);
+        }
+    });
+});
